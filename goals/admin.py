43507600639ca60b7ca6a3187c80e9908a3bfb34
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-26 18:55:48 -0300

    Register Goal model in admin with custom display and search options

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Goal
 # Register your models here.
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('team', 'deadline', 'created_at') 
+    search_fields = ('status', 'description')        
+    list_filter = ('team', 'title')
