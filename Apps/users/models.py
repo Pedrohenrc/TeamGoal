873415954fe64337be9c1182fb59733b55a6fb53
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-26 15:27:02 -0300

    First Commit

---
+++ Apps/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to="profiles/", blank=True, Null=True)
+    role = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__ (self):
+        return self.username
