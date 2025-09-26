1a169cf1c33aebd4b12ecd20ba987798c243a0a9
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-26 20:04:55 -0300

    add contribution to urls

--- contributions/views.py
+++ contributions/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from .models import Contribution
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ContributionForm
 # Create your views here.
+
+class ContributionCreateView (CreateView, LoginRequiredMixin):
+    model = Contribution
+    form_class = ContributionForm
+    sucess_url = reverse_lazy("")
--- goals/urls.py
+++ goals/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", GoalListView.as_view(), name="goal-list"),
     path("create/", GoalCreateView.as_view(), name="goal-create"),
     path("<int:pk>", GoalDetailView.as_view(), name='goal-detail'),
-    path("<int:pk>/editar/", GoalUpdateView.as_view(), name='goal-update')
+    path("<int:pk>/editar/", GoalUpdateView.as_view(), name='goal-update'),
+    path("<int:goal_id>/contributions/", include("contributions.urls"))
 ]
