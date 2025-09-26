ee19b1d669012c585fb08cb0f11c5506d9e77f85
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-26 18:55:21 -0300

    urls for goals

---
+++ goals/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import GoalCreateView, GoalDetailView, GoalListView, GoalUpdateView
+
+urlpatterns = [
+    path("", GoalListView.as_view(), name="goal-list"),
+    path("create/", GoalCreateView.as_view(), name="goal-create"),
+    path("<int:pk>", GoalDetailView.as_view(), name='goal-detail'),
+    path("<int:pk>/editar/", GoalUpdateView.as_view(), name='goal-update')
+]
