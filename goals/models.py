8ac909198f91431440b11bbdc8a4ef485394993e
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-26 18:55:09 -0300

    Views and models for goals

--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from teams.models import Team
+from users.models import CustomUser
 # Create your models here.
+
+class Goal(models.Model):
+    STATUS_CHOICES = [
+        ("pendente"),
+        ("em progresso"),
+        ("concluida")
+    ]
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="goals")
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    deadline = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.team.name})"
--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Goal
+class GoalCreateView(CreateView, LoginRequiredMixin):
+    Model = Goal
+    fields = ['title', 'description', 'deadline', 'status']
+    success_url = reverse_lazy("goal-list")
 
-# Create your views here.
+class GoalListView(ListView, LoginRequiredMixin):
+    Model = Goal
+    context_object_name = "goals"
+
+    def get_queryset(self):
+        queryset = Goal.objects.all()
+
+        status_param = self.request.GET.get("status")
+        if status_param:
+            queryset = queryset.filter(status=status_param)
+
+        team_param = self.request.GET.get("team")
+        if team_param:
+            queryset = queryset.filter(team__id=team_param)
+
+        user_param = self.request.GET.get("user")
+        if user_param:
+            queryset = queryset.filter(contributions__user__id=user_param)
+
+        return queryset
+
+class GoalDetailView (DetailView, LoginRequiredMixin):
+        model = Goal
+        context_object_name = "goal"
+
+class GoalUpdateView (UpdateView, LoginRequiredMixin):
+     model = Goal
+     fields = ['title', 'description', 'deadline', 'status']
+     success_url = reverse_lazy("goat-list")
