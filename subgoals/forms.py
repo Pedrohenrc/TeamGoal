fd4690219c6b56b3752b3c4709d7f15c7607cb6d
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-29 17:15:11 -0300

    Rename SubtaskForm to SubgoalForm; update views to use SubgoalForm and implement Subgoal views for create, detail, update, list, and delete functionalities

--- subgoals/forms.py
+++ subgoals/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Subtask
 
-class SubtaskForm(forms.ModelForm):
+class SubgoalForm(forms.ModelForm):
     class Meta:
         model = Subtask
         fields = ["title", "description", "is_completed", "assigned_to"]
--- subgoals/views.py
+++ subgoals/views.py
@@ -1,13 +1,13 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import SubGoal
 from goals.models import Goal
-from .forms import SubtaskForm
+from .forms import SubgoalForm
 # Create your views here.
-class SubtaskCreateView(LoginRequiredMixin, CreateView):
+class SubgoalCreateView(LoginRequiredMixin, CreateView):
     model = SubGoal
-    form_class = SubtaskForm
+    form_class = SubgoalForm
 
     def form_valid(self, form):
         goal_id = self.kwargs.get("pk")
@@ -16,3 +16,49 @@ class SubtaskCreateView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
          return reverse_lazy('goal-detail', kwargs={"pk": self.kwargs.get("pk")})
+    
+class SubgoalDetailView(LoginRequiredMixin, DetailView):
+    model = SubGoal
+    context_object_name = "subgoal"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contributions'] = self.object.contributions.all()
+
+        return context
+    
+class SubgoalUpdateView(LoginRequiredMixin, UpdateView):
+    model = SubGoal
+    fields = ['title', 'description', 'assigned_to', 'is_completed']
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object.goal.update_progress()
+        return response
+    
+class SubgoalListView(LoginRequiredMixin, ListView):
+    model = SubGoal
+    context_object_name = "subgoals"
+
+    def get_queryset(self):
+        goal_id = self.kwargs["goal_id"]
+        queryset = SubGoal.objects.filter(goal_id=goal_id)
+        user = self.request.GET.get('user')
+        if user:
+            queryset = queryset.filter(assigned_to__id=user)
+        return queryset
+
+class SubgoalDeleteView(DeleteView):
+    model = SubGoal
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        goal = self.object.goal           # pega meta antes de deletar
+        response = super().delete(request, *args, **kwargs)
+        goal.update_progress()            # atualiza progresso depois da exclusão
+        return response
+
+    def get_success_url(self):
+        return reverse_lazy('goal-detail', kwargs={'pk': self.object.goal.id})
+    
+
