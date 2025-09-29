5760d3a17cb0784aec0397db1e3668ff7d7c219e
Pedrohenrc <pirriquecsousa@gmail.com>
2025-09-29 16:00:28 -0300

    Add Subgoal model, views, and URLs; update goal URLs for subtasks

---
+++ subgoals/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from users.models import CustomUser
+from goals.models import Goal
+# Create your models here.
+class SubGoal(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="subtasks")
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    assigned_to = models.ForeignKey(
+        CustomUser, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="subtasks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def fechar_subtask(subtask):
+        subtask.is_completed = True
+        subtask.save()
+        subtask.goal.update_progress()
+
+    def __str__(self):
+        return f"{self.title} ({'Done' if self.is_completed else 'Pending'})"
+    
