afe8a5a0faeffcec2a92b258302a43c0779ba316
Pedrohenrc <pirriquecsousa@gmail.com>
2025-11-05 15:09:26 -0300

    Implementando conftest e tests em goals

--- goals/tests.py
+++ goals/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+# goals/tests.py
+import pytest
+from goals.models import Goal
+from teams.models import Team
+from datetime import datetime, timedelta
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestGoalViewSet:
+    
+    def test_criar_goal(self, usuario_logado):
+        """Testa criar uma meta"""
+        api_client, usuario = usuario_logado
+        
+        # Cria time primeiro
+        team = Team.objects.create(name='Time A', owner=usuario)
+        
+        data = {
+            'team': team.id,
+            'title': 'Meta 1',
+            'description': 'Descrição',
+            'deadline': '2025-12-31',
+            'status': 'pending'
+        }
+        response = api_client.post('/api/goals/', data, format='json')
+        assert response.status_code == 201
+        assert response.data['title'] == 'Meta 1'
+    
+    def test_listar_goals(self, usuario_logado):
+        """Testa listar metas"""
+        api_client, usuario = usuario_logado
+        
+        team = Team.objects.create(name='Time A', owner=usuario)
+        Goal.objects.create(
+            team=team,
+            title='Meta 1',
+            description='Desc',
+            deadline='2025-12-31'
+        )
+        
+        response = api_client.get('/api/goals/')
+        assert response.status_code == 200
