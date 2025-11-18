80a3db955e0e52f65d5c31a055c99f9a33e4fb35
Pedrohenrc <pirriquecsousa@gmail.com>
2025-11-18 20:33:36 -0300

    atualizando as urls

--- subgoals/urls.py
+++ subgoals/urls.py
@@ -4,10 +4,10 @@ from .views import SubgoalCreateView, SubgoalUpdateView, SubgoalDetailView, Subg
 urlpatterns = [
     path("", SubgoalListView.as_view(), name="subgoal-list"),
     path("create/", SubgoalCreateView.as_view(), name="subgoal-create"),
-    path("<int:subgoal_id>", SubgoalDetailView.as_view(), name="subgoal-detail"),
-    path("<int:subgoal_id>/editar", SubgoalUpdateView.as_view(), name="subgoal-update"),
-    path("<int:subgoal_id>/deletar", SubgoalDeleteView.as_view(), name="subgoal-delete" ),
-    path("<int:subgoal_id>/contributions", include("contributions.urls")),
-    path('<int:pk>/toggle-complete/', complete_subtask, name='subgoal-toggle-complete'),
+    path("<uuid:subgoal_id>", SubgoalDetailView.as_view(), name="subgoal-detail"),
+    path("<uuid:subgoal_id>/editar", SubgoalUpdateView.as_view(), name="subgoal-update"),
+    path("<uuid:subgoal_id>/deletar", SubgoalDeleteView.as_view(), name="subgoal-delete" ),
+    path("<uuid:subgoal_id>/contributions", include("contributions.urls")),
+    path('<uuid:pk>/toggle-complete/', complete_subtask, name='subgoal-toggle-complete'),
 
 ]
